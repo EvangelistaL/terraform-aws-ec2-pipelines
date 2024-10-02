5a775ed1b6519a1a5136e461899e00d09aae59db
EvangelistaL <lucas.silva@sptech.school>
2024-10-02 19:45:03 -0300

    wip

--- variables.tf
+++ variables.tf
@@ -3,8 +3,3 @@ variable "aws_region" {
   description = "Region where the resource will be created"
   default     = "us-east-1"
 }
-
-variable "aws_key_pub" {
-  description = "Public key for aws ec2"
-  type        = string
-}
